8f5a1b9494445917a130ae10cea1c4d9c9d6e551
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2023-03-08 01:47:37 -0700

    added Projects and links to Portfolio.js., and added link to resume in Resume.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,17 +4,67 @@ export default function Portfolio() {
     return (
         <div>
             <h1>Portfolio</h1>
-            <p>
-                <ul>
-                    
-                    <li><a href='/Project 1'>Project 1 (Coming Soon!)</a></li>
-                    <li><a href='/Project 2'>Project 2 (Coming Soon!)</a></li>
-                    <li><a href='/Project 3'>Project 3 (Coming Soon!)</a></li>
-                    <li><a href='/Project 4'>Project 4 (Coming Soon!)</a></li>
-                    <li><a href='/Project 5'>Project 5 (Coming Soon!)</a></li>
-                    <li><a href='/Project 6'>Project 6 (Coming Soon!)</a></li>
-                </ul>
-                </p>
+            <ul>
+                <li>
+                    <div>
+                        <h3>Steamin' and Streamin'</h3>
+                        <p>A feature to help the eater</p>
+                        <ul>
+                            <li><a href="https://github.com/FarmFreshYeets/Steamin-And-Streamin">GitHub Repo</a></li>
+                            <li><a href="https://farmfreshyeets.github.io/Steamin-And-Streamin/">Live URL</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h3>Hello Wordl Game</h3>
+                        <p>A word-guessing game</p>
+                        <ul>
+                            <li><a href="https://github.com/uabcHelloWordl/hello-wordl-game">GitHub Repo</a></li>
+                            <li><a href="https://hello-wordl.herokuapp.com/">Live URL</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h3>Work It!</h3>
+                        <p>A workout app to post blogs</p>
+                        <ul>
+                            <li><a href="https://github.com/arellanoe/workit-app">GitHub Repo</a></li>
+                            <li><a href="https://git.heroku.com/work-your-app-off.git">Live URL</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h3>Professional Portfolio</h3>
+                        <ul>
+                            <li><a href="https://github.com/JacquieSue/Professional-Portfolio">GitHub Repo</a></li>
+                            <li><a href="https://jacquiesue.github.io/Professional-Portfolio/">Live URL</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h3>Hello Wordl Game</h3>
+                        <p>A word-guessing game</p>
+                        <ul>
+                            <li><a href="https://github.com/uabcHelloWordl/hello-wordl-game">GitHub Repo</a></li>
+                            <li><a href="https://hello-wordl.herokuapp.com/">Live URL</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h3>Hello Wordl Game</h3>
+                        <p>A word-guessing game</p>
+                        <ul>
+                            <li><a href="https://github.com/uabcHelloWordl/hello-wordl-game">GitHub Repo</a></li>
+                            <li><a href="https://hello-wordl.herokuapp.com/">Live URL</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
         </div>
     );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,40 +2,40 @@ import React from 'react';
 // import LoginForm from '../LoginForm';
 
 export default function Resume() {
-    console.log('Hello -> loggedIn, loggedIn');
+    // console.log('Hello -> loggedIn, loggedIn');
 
     return (
         <div>
-            {/* {loggedIn ? ( */}
+            {/* {loggedIn} ? ( */}
                 <div>
-                    <h3>Thank you visiting!</h3>
-                    <a href='/Resume'>View my Resume(COMING SOON!)</a>
+                    <h3>Thank you for visiting!</h3>
+                    <a href='https://drive.google.com/file/d/1ceksYAT0t_o6lC9TwQ7F2-Te1Eo0_-Vy/view?usp=sharing'>View my Resume</a>
                     <p>
                         Front end:
-                            <ul>
-                                HTML,
-                                CSS,
-                                JavaScript,
-                                JQuery,
-                                responsive design,
-                                React,
-                                Bootstrap
-                            </ul>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>JQuery</li>
+                            <li>responsive design</li>
+                            <li>React</li>
+                            <li>Bootstrap</li>
+                        </ul>
                     </p>
                     <p>
                         Back end:
-                            <ul>
-                                APIs,
-                                Node,
-                                Express,
-                                MySQL, Sequelize,
-                                MongoD, Mongoose,
-                                REST,
-                                GraphQL
-                            </ul>
+                        <ul>
+                        <li>APIs</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySQL, Sequelize</li>
+                        <li>MongoD, Mongoose</li>
+                        <li>REST</li>
+                        <li>GraphQL</li>
+                        </ul>
                     </p>
                 </div>
-            {/* )} */}
+            {/* ) */}
         </div>
     )
 }
