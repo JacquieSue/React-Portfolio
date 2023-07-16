d5c2c6a38a65fb01fa7c73dacdce32152eaf7b3c
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2023-07-16 14:57:27 -0700

    rearranging files, added alert.js

--- my-react-portfolio/src/components/Nav.js
+++ my-react-portfolio/src/components/Nav.js
@@ -4,32 +4,44 @@ export default function Nav() {
   const linkStyle = { border: '1px black', padding: '5px' };
 
   return (
-    <header>
-      <h1 style="color:purple;">Jacquelyn Suzette Pulliam</h1>
-      <nav className="main-header-menu">
-        <section
-          style={{
-            display: 'flex',
-            fontFamily: 'helvetica',
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            justifyContent: 'center',
-          }}
-        >
-          <div style={linkStyle}>
-            <a href="#">About Me</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Portfolio</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Resume</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Contact</a>
-          </div>
-        </section>
-      </nav>
-    </header>
-  );
+    <nav className="main-header-menu">
+      <section
+        style={{
+          display: 'flex',
+          fontFamily: 'helvetica',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <header>
+          <h1>Jacquelyn Suzette Pulliam</h1>
+          <nav className="main-header-menu">
+            <section
+              style={{
+                display: 'flex',
+                fontFamily: 'helvetica',
+                flexDirection: 'row',
+                alignItems: 'flex-center',
+                justifyContent: 'flex-center',
+              }}
+            >
+              <div style={linkStyle}>
+                <a href="#">About Me</a>
+              </div>
+              <div style={linkStyle}>
+                <a href="#">Portfolio</a>
+              </div>
+              <div style={linkStyle}>
+                <a href="#">Resume</a>
+              </div>
+              <div style={linkStyle}>
+                <a href="#">Contact</a>
+              </div>
+            </section>
+          </nav>
+        </header>
+      </section>
+    </nav>
+  )
 }
